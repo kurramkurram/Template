0ecd696baf73c110e49c57c08a32cdd18bfbc891
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-04-01 23:57:23 +0900

    [Update] Main view

--- app/src/main/java/io/github/kurramkurram/template/viewmodel/MainViewModel.kt
+++ app/src/main/java/io/github/kurramkurram/template/viewmodel/MainViewModel.kt
@@ -2,15 +2,31 @@ package io.github.kurramkurram.template.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import io.github.kurramkurram.template.model.SampleUseCase
+import io.github.kurramkurram.template.util.showLongToast
 
 /**
  * Main fragment view model.
  */
 class MainViewModel(private val application: Application) : AndroidViewModel(application) {
-    private val sampleUseCase: SampleUseCase = SampleUseCase()
+    private val sampleUseCase: SampleUseCase = SampleUseCase(application.applicationContext)
 
-    fun getWordList() {
-        sampleUseCase.shuffleWord()
+    val word = MutableLiveData("")
+    val shuffledWord = MutableLiveData("")
+
+    fun shuffleWord() {
+        word.value?.let {
+            shuffledWord.value = sampleUseCase.shuffleWord(it)
+        }
+    }
+
+    fun saveWord() {
+        word.value?.let { origin ->
+            shuffledWord.value?.let { shuffled ->
+                sampleUseCase.saveWord(origin, shuffled)
+                application.applicationContext.showLongToast("保存しました")
+            }
+        }
     }
 }
