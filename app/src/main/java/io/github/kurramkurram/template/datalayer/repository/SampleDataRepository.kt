0bcc8be52e26f388cd54d43a31a475728f7c3e00
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-04-01 23:57:01 +0900

    [Update] repository

--- app/src/main/java/io/github/kurramkurram/template/datalayer/repository/SampleDataRepository.kt
+++ app/src/main/java/io/github/kurramkurram/template/datalayer/repository/SampleDataRepository.kt
@@ -1,5 +1,7 @@
 package io.github.kurramkurram.template.datalayer.repository
 
+import android.content.Context
+import io.github.kurramkurram.template.datalayer.data.SampleWord
 import io.github.kurramkurram.template.datalayer.datasource.SampleDataSource
 import io.github.kurramkurram.template.datalayer.datasource.SampleDataSourceImpl
 
@@ -10,15 +12,34 @@ import io.github.kurramkurram.template.datalayer.datasource.SampleDataSourceImpl
  */
 abstract class SampleRepository {
 
-    abstract fun fetchAllWords()
+    /**
+     * save to datasource.
+     *
+     * @param data [SampleWord]
+     */
+    abstract fun save(data: SampleWord)
 
+    /**
+     * get all data.
+     *
+     * @return [SampleWord]'s list.
+     */
+    abstract fun fetchAllWords(): List<SampleWord>
+
+    /**
+     * get a data which matched search word.
+     *
+     * @param word search word
+     * @return search result
+     */
+    abstract fun fetchWord(word: String): SampleWord
 }
 
 class SampleRepositoryImpl(
-    private val sampleDataSource: SampleDataSource = SampleDataSourceImpl()
+    context: Context,
+    private val sampleDataSource: SampleDataSource = SampleDataSourceImpl(context)
 ) : SampleRepository() {
-
-    override fun fetchAllWords() {
-
-    }
+    override fun save(data: SampleWord) = sampleDataSource.insert(data)
+    override fun fetchAllWords(): List<SampleWord> = sampleDataSource.selectAll()
+    override fun fetchWord(word: String): SampleWord = sampleDataSource.selectWord(word)
 }
