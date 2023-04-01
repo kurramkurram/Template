22e25212b17fadc499a7fdb4f2beba8f5f449d20
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-04-01 23:56:07 +0900

    [Update] Datasource

--- app/src/main/java/io/github/kurramkurram/template/datalayer/datasource/SampleDataSource.kt
+++ app/src/main/java/io/github/kurramkurram/template/datalayer/datasource/SampleDataSource.kt
@@ -1,5 +1,12 @@
 package io.github.kurramkurram.template.datalayer.datasource
 
+import android.content.Context
+import io.github.kurramkurram.template.datalayer.data.SampleWord
+import io.github.kurramkurram.template.datalayer.source.database.SampleDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 /**
  * This datasource is sample.
  * You can create new datasource class or use this datasource class.
@@ -7,12 +14,48 @@ package io.github.kurramkurram.template.datalayer.datasource
  */
 abstract class SampleDataSource {
 
-    abstract fun selectAll()
+    /**
+     * insert to database.
+     *
+     * @param sampleWord [SampleWord]
+     */
+    abstract fun insert(sampleWord: SampleWord)
+
+    /**
+     * get all data.
+     *
+     * @return [SampleWord]'s list.
+     */
+    abstract fun selectAll(): List<SampleWord>
+
+    /**
+     * get a data which matched search word.
+     *
+     * @param word search word
+     * @return search result
+     */
+    abstract fun selectWord(word: String): SampleWord
 }
 
-class SampleDataSourceImpl() : SampleDataSource() {
+class SampleDataSourceImpl(
+    context: Context,
+    private val database: SampleDatabase = SampleDatabase.getDatabases(context)
+) : SampleDataSource() {
 
-    override fun selectAll() {
-        TODO("Not yet implemented")
+    override fun insert(sampleWord: SampleWord) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val dao = database.sampleWordDao()
+            dao.insert(sampleWord = sampleWord)
+        }
+    }
+
+    override fun selectAll(): List<SampleWord> {
+        val dao = database.sampleWordDao()
+        return dao.selectAll()
+    }
+
+    override fun selectWord(word: String): SampleWord {
+        val dao = database.sampleWordDao()
+        return dao.selectWord(word)
     }
 }
