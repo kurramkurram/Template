4fc5badbb57d4857a0b0bc1371dbf0dd056555af
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2023-04-02 16:29:28 +0900

    [Update] binding adapter

--- app/src/main/java/io/github/kurramkurram/template/util/BindingAdapters.kt
+++ app/src/main/java/io/github/kurramkurram/template/util/BindingAdapters.kt
@@ -1,9 +1,17 @@
 package io.github.kurramkurram.template.util
 
+import android.widget.Button
+import androidx.databinding.BindingAdapter
+
 /**
  * Binding Adapter.
  * Update UI by data binding.
  */
 object BindingAdapters {
 
+    @BindingAdapter(value = ["word"], requireAll = true)
+    @JvmStatic
+    fun enableButton(button: Button, word: String) {
+        button.isEnabled = word.isNotBlank()
+    }
 }
